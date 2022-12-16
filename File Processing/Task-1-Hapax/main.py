17a8cebeaae96ad83045d8e38c156549156bf489
Thomas Gozalie <114067350+regencode@users.noreply.github.com>
2022-12-16 11:28:40 +0700

    Add files via upload

--- File Processing/Task-1-Hapax/main.py
+++ File Processing/Task-1-Hapax/main.py
@@ -1,34 +1,35 @@
 import os;
 
-if os.getcwd != "Task-1-Hapax":
+if os.getcwd != "Task-1-Hapax": #to change directory to correct directory
     os.chdir("Task-1-Hapax")
     
 def removePunctuations(string):
     punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     for letter in string:
         if letter in punctuations:
-            string = string.replace(letter, "")
+            string = string.replace(letter, "") #replace punctuations with empty string
     return string
 
-
+    
 def returnHapaxes(filename):
 
     fileReader = open(filename, "r+")
     fileWriter = open("hapaxList.txt", "w")
-    wordList = {}
+    wordList = {} #use dict so i can store words as keys to remove duplicated automatically
 
     for lines in fileReader.readlines():
         words = lines.split()
         for word in words:
-            word.lower()
+            word = word.lower()
             word = removePunctuations(word)
-            wordList.setdefault(word)
+            wordList.setdefault(word) #append words to keys of wordList, if already exists then don't append
 
     for words in wordList.keys():
-        fileWriter.write(words + " ")
+        fileWriter.write(words + " ") #write each key in wordList
 
     fileReader.close()
     fileWriter.close()
+    print("Program finished")
 
 
 def main():
--- File Processing/Task-2-sentenceSplitter/main.py
+++ File Processing/Task-2-sentenceSplitter/main.py
@@ -5,10 +5,10 @@ if os.getcwd != "Task-2-sentenceSplitter":
 
 def checkEndOfSentence(word):
     ends = ".?!"
-    exceptions = ["mr.", "mrs.", "dr.", "i.e.", "jr."]
+    exceptions = ["mr.", "mrs.", "dr.", "i.e.", "jr."] #titles so they are not end of sentence
     if word.lower() in exceptions:
         return False
-    elif word[-1] in ends:
+    elif word[-1] in ends: #if end of word is any of the punctuations that marks the end of a sentence, return True
         return True
     else:
         return False
@@ -17,11 +17,11 @@ def sentenceSplitter(filename):
     fileReader = open(filename, "r")
     fileWriter = open("output.txt", "w")
 
-    for lines in fileReader.readlines():
-        words = lines.split()
+    for lines in fileReader.readlines(): #read each line
+        words = lines.split()  #turn line into list of words
         for word in words:
-            fileWriter.write(word + " ")
-            if checkEndOfSentence(word) == True:
+            fileWriter.write(word + " ") #write to output
+            if checkEndOfSentence(word) == True: #if word marks end of sentence, add newline character
                 fileWriter.write("\n")
     
     fileReader.close()
